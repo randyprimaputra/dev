3da72cc4e3e09c1d2692620423ebb930bc7c01b8
RandyPrimaPutra <103095236+randyprimaputra@users.noreply.github.com>
2022-11-09 23:59:23 +0800

    Make a comment
    making a comment

---
+++ c++/Comment/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+
+    // inline comment
+    /*
+       multi line comment
+       like
+       this
+    */
+    std::cout << "Membuat comment" << std::endl;
+    
+    return 0;
+}
