a59377df89855c55f7eba099df6db7e307c5b05d
RandyPrimaPutra <103095236+randyprimaputra@users.noreply.github.com>
2022-12-21 11:50:19 +0800

    Update conditional_expression.dart

--- dart/Conditional Expressions/lib/conditional_expression.dart
+++ dart/Conditional Expressions/lib/conditional_expression.dart
@@ -1,4 +1,4 @@
-void main (){
+void main() {
   /* Conditional Expressions
   * 1. condition ? exp1 : exp2
   * If condition is true, evaluates expr1 (and returns its value)
@@ -7,15 +7,27 @@ void main (){
 
   int a = 2;
   int b = 3;
-  int c = 10;
 
-  if (a >b) {
+  int smallNumber = a < b ? a : b;
+/*
+  if (a > b) {
     print("$a is smaller");
   } else {
     print("$b is smaller");
   }
 
   // another way
-  a < b? print("$a is smaller") : print ("$b is smaller");
+  a < b ? print("$a is smaller") : print("$b is smaller");
+  */
+  print("$smallNumber is smaller");
 
+  /* 2. exp1 ?? exp2
+  * If expr1 is non-null, return its value; otherwise, evaluates and
+  * returns the value of expr2.return*/
+
+  String? name = "Tom";
+  //name = "tio";
+
+  String nameToPrint = (name as String?) ?? "Guest User";
+  print(nameToPrint);
 }
