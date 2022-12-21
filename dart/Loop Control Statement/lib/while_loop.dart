366d1adb398f3637bcded932a8ca7dfdbe9644d0
RandyPrimaPutra <103095236+randyprimaputra@users.noreply.github.com>
2022-12-21 13:44:26 +0800

    Create while_loop.dart

---
+++ dart/Loop Control Statement/lib/while_loop.dart
@@ -0,0 +1,12 @@
+void main(){
+
+  // WHILE LOOP
+  // WAP to find the even numbers between 1 to 10
+  int i = 1;
+  while (i <= 10) {
+    if (i % 2 == 0) {
+      print(i);
+    }
+    i++;
+  }
+}
