2577eee6e5e8e60e5e88c2e8763890ec883825ad
RandyPrimaPutra <103095236+randyprimaputra@users.noreply.github.com>
2022-12-23 01:00:37 +0800

    Update optional_named_parameter.dart

--- dart/functions or method/lib/optional_named_parameter.dart
+++ dart/functions or method/lib/optional_named_parameter.dart
@@ -1,3 +1,11 @@
+
+// Optional Named Parameters
 void main(){
 
+int result = findVolume(2, breadth: 10, height: 20);
+print(result);
+
+}
+ int findVolume( var length, { var breadth, var height}){
+  return length * breadth * height;
 }
