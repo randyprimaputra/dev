275527114f8af45c06bf03d0a6adc7541465bae5
RandyPrimaPutra <103095236+randyprimaputra@users.noreply.github.com>
2023-01-05 14:10:14 +0800

    update

--- dart/Exception Handling/lib/customexceptionhandling.dart
+++ dart/Exception Handling/lib/customexceptionhandling.dart
@@ -1,3 +1,21 @@
-void main(){
+void main() {
+  // Custom Exception
+  try {
+    depositMoney(-200);
+  } catch (e) {
+    print(e.toString());
+  } 
+}
 
+
+class DepositException implements Exception {
+  String toString() {
+    return "Your cannot enter amount less than 0";
+  }
+}
+
+void depositMoney(int amount) {
+  if (amount < 0) {
+    throw new DepositException();
+  }
 }
