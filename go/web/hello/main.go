f2f97c1b3206a3c8b20aca666ce3a27497a39780
RandyPrimaPutra <103095236+randyprimaputra@users.noreply.github.com>
2023-12-14 23:24:07 +0800

    update

--- go/web/hello/main.go
+++ go/web/hello/main.go
@@ -2,14 +2,54 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"net/http" //package web http
+	"time"
 )
 
-func helloWorldPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "hello World")
+func HandleFunc(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprint(w, "hello World")
+	switch r.URL.Path {
+	case "/":
+		fmt.Fprint(w, "Hello World")
+	case "/ninja":
+		fmt.Fprint(w, "Wallace")
+	default:
+		fmt.Fprint(w, "Big Fat Error!")
+	}
+
+	fmt.Printf("Handling function with %s request\n", r.Method)
+}
+
+func htmlVsPlain(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("htmlVsPlain")
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Hello World</h1>")
+}
+
+func timeout(w http.ResponseWriter, r *http.Request) { //fungsi timeout
+	fmt.Println("Timeout attempt")
+	time.Sleep(2 * time.Second)
+	fmt.Fprint(w, "Did *not* timeout")
+}
+
+func HelloWorldNinjaMode(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("helloWorldNinjaMode")
+	fmt.Fprint(w, "<h1 style=\"background-color:grey;\">Hello World</h1>")
 }
 
 func main() {
-	http.HandleFunc("/", helloWorldPage)
-	http.ListenAndServe("", nil)
+	http.HandleFunc("/", htmlVsPlain)
+	http.HandleFunc("/timeout", timeout)
+	server := http.Server{
+		Addr:         "",
+		Handler:      nil,
+		ReadTimeout:  1000,
+		WriteTimeout: 1000,
+	}
+
+	var muxNinjaMode http.ServeMux
+	server.Handler = &muxNinjaMode
+	muxNinjaMode.HandleFunc("/", HelloWorldNinjaMode)
+
+	server.ListenAndServe() // running localhost
 }
