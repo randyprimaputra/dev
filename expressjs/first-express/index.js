2e8eb6800001f3fc4ab76bc2f2c0cbfb08462726
RandyPrimaPutra <103095236+randyprimaputra@users.noreply.github.com>
2024-03-05 08:22:01 +0800

    initial

--- expressjs/first-express/index.js
+++ expressjs/first-express/index.js
@@ -2,9 +2,30 @@ const express = require('express');
 
 const app = express();
 
+/*
 app.use((req, res) => {
     console.log('We got Request');
     res.send('<h1>Hello Express</h1>');
+});
+*/
+
+app.get('/', (req, res) =>{
+    console.log('we got request');
+    res.send('this is Home page');
+});
+
+app.get('/cats', (req, res) =>{
+    console.log('we got request');
+    res.send('this is cats page');
+});
+
+app.get('/blog/:judul/:author',(req, res) => {
+    const {judul, author} = req.params;
+    res.send(`ini postingan dengan judul: ${judul}|${author}`);
+});
+
+app.get('*', (req, res) => {
+    res.send('Halaman tidak di temukan');
 });
 
 app.listen(8080, () => {
