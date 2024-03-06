4a677819508dcda6066520787d69c47859973593
RandyPrimaPutra <103095236+randyprimaputra@users.noreply.github.com>
2024-03-06 09:54:12 +0800

    initial

--- go/fyne-gui/main.go
+++ go/fyne-gui/main.go
@@ -1,9 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
 
 func main() {
 
-	fmt.Println("Test Fyne...")
+	//new app
+	a := app.New()
 
+	// new window
+	w := a.NewWindow(" tittle bar")
+
+	//first widget
+	c := widget.NewLabel("text")
+
+	w.SetContent(c)
+
+	w.ShowAndRun()
 }
