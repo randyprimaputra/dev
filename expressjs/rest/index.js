b551fd9ed507b2d41dcace3a0b3c45a56d85608b
RandyPrimaPutra <103095236+randyprimaputra@users.noreply.github.com>
2024-03-10 10:50:38 +0800

    initial

--- expressjs/rest/index.js
+++ expressjs/rest/index.js
@@ -1,14 +1,16 @@
 const express = require('express')
-
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended:true}))
 
 app.get('/order', (req,res)=>{
     res.send('GET order response')
 });
 
 app.post('/order', (req,res)=>{
-    console.log(req.body)
-    res.send('POST order response')
+    const {item, qty } = req.body
+    res.send(`Item: ${item} - Qty: ${qty}`)
 });
 
 app.listen(8080, ()=> {
